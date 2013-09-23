f5db696eb177cb598e29ee6096a06fc6b04c9e8c
Reuben H Wilson <reuben.wilson@cyn-reub.com>
2013-09-22 23:01:05 -0500

    Continue saving PrayerRespone

--- iOS/PrayerRequestJournal/PrayerRequestJournal/PRJNewResponseViewController.h
+++ iOS/PrayerRequestJournal/PrayerRequestJournal/PRJNewResponseViewController.h
@@ -11,15 +11,13 @@
 
 #import <UIKit/UIKit.h>
 #import "PrayerResponse.h"
+#import "PrayerRequest.h"
 
 @interface PRJNewResponseViewController : UITableViewController<UITextViewDelegate>
 
+@property (weak, nonatomic) PrayerResponse *prayerResponse;
+@property (weak, nonatomic) PrayerRequest *prayerRequest;
 
-@property (strong, nonatomic) PrayerResponse *prayerResponse;
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-
-@property (nonatomic, retain) IBOutlet UISegmentedControl * dispositionsSegmentedControl;
-@property (nonatomic, retain) IBOutlet UITextView * responseDetail;
-
 
 @end
--- iOS/PrayerRequestJournal/PrayerRequestJournal/PRJNewResponseViewController.m
+++ iOS/PrayerRequestJournal/PrayerRequestJournal/PRJNewResponseViewController.m
@@ -18,9 +18,8 @@
 
 @implementation PRJNewResponseViewController
 @synthesize managedObjectContext;
-@synthesize dispositionsSegmentedControl;
-@synthesize responseDetail;
 @synthesize prayerResponse;
+@synthesize prayerRequest;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -43,23 +42,53 @@
 	
     sections = @[@"Disposition", @"Response"];
     
+    UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(save:)];
+    
+    self.navigationItem.rightBarButtonItem = saveButton;
+    
+    // [self loadData];
+}
+
+- (void) loadDisposition: (UISegmentedControl *)segmentedControl{
+    if( self.prayerResponse != nil){
+        
+        if( [self.prayerResponse.response compare:@"No"] == 0 ){
+            [segmentedControl setSelectedSegmentIndex:0];
+        }
+        else if ( [self.prayerResponse.response compare:@"Yes"] == 0){
+            [segmentedControl setSelectedSegmentIndex:1];
+        }
+        else if ( [self.prayerResponse.response compare:@"Waiting"] == 0 ){
+            [segmentedControl setSelectedSegmentIndex:2];
+        }
+    }
 }
 
 -(void) loadData{
-
+    UISegmentedControl * segControlDisposition = (UISegmentedControl *)[self.view viewWithTag:kDispostionTag];
+    UITextView * responseTextView = (UITextView *)[self.view viewWithTag:kResponseTag];
+    
+    if(nil == segControlDisposition || nil == responseTextView){
+        return ;
+    }
+    
 	if( self.prayerResponse != nil )
 	{
-		
+        if( [self.prayerResponse.response compare:@"No"] == 0 ){
+            [segControlDisposition setSelectedSegmentIndex:0];
+        }
+        else if ( [self.prayerResponse.response compare:@"Yes"] == 0){
+            [segControlDisposition setSelectedSegmentIndex:1];
+        }
+        else if ( [self.prayerResponse.response compare:@"Waiting"] == 0 ){
+            [segControlDisposition setSelectedSegmentIndex:2];
+        }
         
-	
-	
+        responseTextView.text = self.prayerResponse.response;
 	}
 }
 
--(void) save
-{
-	//[activeInputView resignFirstResponder];
-	
+-(void) save:(id) sender {
     NSManagedObjectContext *moc = [self managedObjectContext];
     
     NSError *error = nil;
@@ -67,33 +96,33 @@
     PrayerResponse * response = nil;
 	
 	if( nil == prayerResponse ) {
-	
 		response = (PrayerResponse *)[NSEntityDescription insertNewObjectForEntityForName:@"PrayerResponse" inManagedObjectContext:moc];
-		
         response.dateEntered = [NSDate date];
+        response.request = prayerRequest;
 	}
 	else {
 	
 		response = prayerResponse;
 	}
     
-    //UITextField *textField = (UITextField *) [self.view viewWithTag:kRequestTitleTag];
-    //UITextView *textView = (UITextView *) [self.view viewWithTag:kRequestDetailsTag];
+    UITextView * textView = (UITextView *)[self.view viewWithTag:kResponseTag];
+    UISegmentedControl * segControl1 = (UISegmentedControl *)[self.view viewWithTag:kDispostionTag];
 
-    //NSString *requestTitle = [textField text];
-    //NSString *requestDetail = [textView text];
-    
-    //if([requestTitle length] > 0 && [requestDetail length] > 0 ){
-		
-    response.response  = @"";
-    response.disposition = @"";
-    //response.dateRequested = dateRequested;
-    //pr.requestor = prayerRequestor;
+    response.response  = [textView text];
+    NSInteger index = [segControl1 selectedSegmentIndex];
+
+    if( index == 0 ){
+        response.disposition = @"Yes";
+    }else if (index == 1 ){
+        response.disposition = @"No";
+    }
+    else{
+        response.disposition = @"Waiting";
+    }
     
     [moc save:&error];
 		
-    if(nil != error)
-    {
+    if(nil != error){
         NSLog(@"Error occurred saving prayer request");
     }
         
@@ -104,9 +133,7 @@
     [view show];
     
     view = nil;
-    //}
-	
-
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -135,12 +162,13 @@
 {
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     
     // Configure the cell...
-    switch( [indexPath section])
+    switch([indexPath section])
     {
         case 0:
             [self configureDispositionCell:cell prayerResponse:self.prayerResponse];
@@ -211,21 +239,36 @@
  
  */
 
- -(void) configureResponseCell:(UITableViewCell*)cell prayerResponse:(PrayerResponse*) response
- {
+-(CGFloat)tableView:(UITableView *) tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    CGFloat cellHeight = 40.0;
+    
+    if( [indexPath section] == 1 ){
+        cellHeight = 150.0;
+    }
+    
+    return cellHeight;
+}
+
+ -(void) configureDispositionCell:(UITableViewCell*)cell prayerResponse:(PrayerResponse*) response{
      
      NSArray * dispositions = @[@"Yes, continue", @"No, not for me", @"Waiting"];
      
      UISegmentedControl * dispositionSegmentedControl = [[UISegmentedControl alloc] initWithItems:dispositions];
      
+     [dispositionSegmentedControl setSelectedSegmentIndex:1];
+     
      dispositionSegmentedControl.tag = kDispostionTag;
      
-     [cell.contentView addSubview:dispositionSegmentedControl];
+     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(5,5,320,20)];
+     [view addSubview:dispositionSegmentedControl];
+     [self loadDisposition:dispositionSegmentedControl];
+     
+     [cell.contentView addSubview:view];
  }
 
- -(void) configureDispositionCell:(UITableViewCell*)cell prayerResponse:(PrayerResponse*) response
- {
-     UITextView * responseView = [[UITextView alloc] initWithFrame:CGRectMake(5,5,290,100)];
+ -(void) configureResponseCell:(UITableViewCell*)cell prayerResponse:(PrayerResponse*) response{
+     UITextView * responseView = [[UITextView alloc] initWithFrame:CGRectMake(1,1,290,145)];
      
      responseView.keyboardType = UIKeyboardTypeDefault;
      responseView.returnKeyType = UIReturnKeyDefault;
@@ -233,47 +276,80 @@
      responseView.delegate = self;
      responseView.tag = kResponseTag;
      
-     /*
-     UIToolbar *toolbar = [[UIToolbar alloc] init];
-     [toolbar setBarStyle:UIBarStyleBlackTranslucent];
-     [toolbar sizeToFit];
-     
-     NSArray *itemsArray = @[
-                             [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil],
-                             [[UIBarButtonItem alloc] initWithTitle:@"Title" style:UIBarButtonItemStyleBordered target:self action:@selector(goToTitleField)],
-                             [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleBordered target:self action:@selector(goToFinishedEditing)]
-                             ];
-     
-     [toolbar setItems:itemsArray];
-     */
      [responseView setInputAccessoryView:[self keyboardToolBar]];
      
      if( nil != response){
-         
          responseView.text = response.response;
      }
-
+     
      [cell.contentView addSubview:responseView];
-    
  }
 
 - (UIToolbar *)keyboardToolBar {
     
     UIToolbar *toolbar = [[UIToolbar alloc] init];
-    [toolbar setBarStyle:UIBarStyleBlackTranslucent];
+    
+    [toolbar setBarStyle:UIBarStyleDefault];
     [toolbar sizeToFit];
     
-    segControl = [[UISegmentedControl alloc] initWithItems:@[@"Previous", @"Next"]];
-    [segControl setSegmentedControlStyle:UISegmentedControlStyleBar];
+    UIBarButtonItem *nextButton = [[UIBarButtonItem alloc]
+                                   initWithTitle:@"Next" style:UIBarButtonItemStyleBordered target:self action:@selector(moveFocusToDisposition:)];
+    
+    
+    
+    
+    // [nextButton setTintColor: [UIColor lightGrayColor]];
+    
+    UIBarButtonItem *flexButton = [[UIBarButtonItem alloc]
+                                   initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    
+    UIBarButtonItem *doneButton =[[UIBarButtonItem alloc]
+                                  initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(resignKeyboard:)];
+    
+    NSArray *itemsArray = @[nextButton, flexButton, doneButton];
+    
+    [toolbar setItems:itemsArray];
+    
+    return toolbar;
+}
+
+- (void) moveFocusToDisposition:(id)sender {
+    
+    UISegmentedControl * dispositionView = (UISegmentedControl *)[self.view viewWithTag:kDispostionTag];
+    UITextView * responseView = (UITextView *)[self.view viewWithTag:kResponseTag];
+    
+    [responseView resignFirstResponder];
+    [dispositionView becomeFirstResponder];
+}
+
+- (void) resignKeyboard:(id)sender{
+    
+    UITextView * responseView = (UITextView *)[self.view viewWithTag:kResponseTag];
+    
+    [responseView resignFirstResponder];
+}
+
+
+/*
+
+- (UIToolbar *)keyboardToolBar3 {
+    
+    UIToolbar *toolbar = [[UIToolbar alloc] init];
+    [toolbar setBarStyle:UIBarStyleDefault];
+    [toolbar sizeToFit];  // UIBarStyleBlackTranslucent
+    
+    segControl = [[UISegmentedControl alloc] initWithItems:@[@"Previous", @"Next", @"Done"]];
+    [segControl setSegmentedControlStyle:UIBarButtonSystemItemAction];
     segControl.momentary = YES;
     
     [segControl addTarget:self action:@selector(changeRow:) forControlEvents:(UIControlEventValueChanged)];
     [segControl setEnabled:NO forSegmentAtIndex:0];
     
     UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithCustomView:segControl];
+    // UIBarButtonItem *previousButton = [[UIBarButtonItem alloc] initWithCustomView:segControl];
     
+    // NSArray *itemsArray = @[previousButton, nextButton];
     NSArray *itemsArray = @[nextButton];
-    
     [toolbar setItems:itemsArray];
     
     return toolbar;
@@ -283,14 +359,22 @@
     
     int index = [sender selectedSegmentIndex];
     
-    //if (idx) {
-    //    self.topText.text = @"Top one";
-    //    [self.bottomText becomeFirstResponder];
-    //}
-    //else {
-    //    self.bottomText.text =@"Bottom one";
-    //    [self.topText becomeFirstResponder];
-    //}
+    switch (index) {
+        case 1:
+        {
+            UITextView *textView = (UITextView *) [self.view viewWithTag:kResponseTag];
+            
+            [textView resignFirstResponder];
+            
+            UISegmentedControl * dispositionSegmentedControl = (UISegmentedControl *)[self.view viewWithTag:kDispostionTag];
+            [dispositionSegmentedControl becomeFirstResponder];
+        }
+        break;
+            
+    default:
+        break;
+    }
 }
+*/
 
 @end
