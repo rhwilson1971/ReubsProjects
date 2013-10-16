5a273d569977044902af8b6db969f5ad28d5c45f
Reuben Wilson <reuben.wilson@wilmss.com>
2013-10-16 13:15:40 -0500

    Add model updates

---
+++ web/LegoList/LegoList/Models/LegoProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LegoList.Models
+{
+    public enum SatisfactionLevel { Low, Medium, High };
+
+    public class LegoProduct
+    {
+        public int Id {get ;set ;}
+        public string Description { get; set; }
+        public string ProductId { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public SatisfactionLevel SatisfactionLevel { get; set; }
+    }
+}
